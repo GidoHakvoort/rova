d5b7b47873be2295c619af571b0844ac621d434a
synoniem@hotmail.com <synoniem@hotmail.com>
2022-02-19 16:34:54 +0100

    Rewritten because significant changed ROVA API

--- rova/rova.py
+++ rova/rova.py
@@ -1,6 +1,10 @@
 """
-Wrapper to get ROVA calendar from Rova's Mobile App or Website
-See https://www.rova.nl/
+Wrapper to get ROVA calendar from Rova's API
+Acces to this ROVA API has been simplified since version 0.2.1 of this wrapper
+Just use https://www.rova.nl/api/waste-calendar/upcoming?postalcode=1000AA&houseNumber=1&addition=&take=5
+with a existing combination of postalcode, housenumber, housenumber addition
+
+Be aware that this API has not been officially published by ROVA.
 """
 
 from datetime import datetime
@@ -9,211 +13,76 @@ import random
 import requests
 
 __title__ = "rova"
-__version__ = "0.2.1"
-__author__ = "Gido Hakvoort"
+__version__ = "0.3.0"
+__author__ = "Gido Hakvoort and synoniem <synoniem@hotmail.com>"
 __license__ = "MIT"
-
-ROVA_SOURCE_SITE = 'site'
-ROVA_SOURCE_API = 'api'
 
 class Rova:
     """
-    ROVA class directing requests to provided datasource
+    ROVA class
     """
 
-    def __init__(self, zip_code, house_number, house_addition='', source=ROVA_SOURCE_API):
+    def __init__(self, zip_code, house_number, house_addition="",take=5):
 
-        if source == ROVA_SOURCE_SITE:
-            self.source = RovaSourceWebsite(zip_code, house_number, house_addition)
-        else:
-            self.source = RovaSourceAPI(zip_code, house_number, house_addition)
-
-    def is_rova_area(self):
-        """
-        Check with data source if ROVA collects garbage at this address
-        """
-        return self.source.is_rova_area()
-
-    def get_calendar_items(self):
-        """
-        Request ROVA calendar items from selected data source
-        """
-        return self.source.get_calendar_items()
-
-
-class AbstractRovaSource:
-    """
-    Abstract class holding location data
-    """
-
-    def __init__(self, zip_code, house_number, house_addition):
         """
         To fetch the garbage calendar, you need to set a zip_code and house_number.
         """
         self.zip_code = zip_code.replace(' ', '')
         self.house_number = house_number.strip()
         self.house_addition = house_addition.strip()
+        self.take = int(take)
 
     def is_rova_area(self):
         """
         Check if ROVA collects garbage at this address
         """
-
-    def get_calendar_items(self):
-        """
-        Request ROVA calendar items
-        """
-
-
-class RovaSourceWebsite(AbstractRovaSource):
-    """
-    DataSource using ROVA's Website-method.
-    See https://www.rova.nl/
-    """
-
-    def __init__(self, zip_code, house_number, house_addition):
-        """
-        A random id will automatically be generated
-        """
-        super().__init__(zip_code, house_number, house_addition)
-        self.rova_id = random.randint(10000, 30000)
-
-    def is_rova_area(self):
-        """
-        Check if ROVA collects garbage at this address
-        """
-        url = 'https://www.rova.nl/api/visitor'
-
-        # request data from rova server and check if rova collects garbage at this address
-        response = requests.post(url, data = {
-            'HouseNumber': self.house_number,
-            'ZipCode': self.zip_code,
-            'Ip': 'b',
-            'Portal': 'inwoners',
-            'UserAgent': 'text'
-            })
-
-        response.raise_for_status()
-
-        rova_response = response.json()
-
-        return rova_response.get('IsRovaArea')
-
-    def get_calendar_items(self):
-        """
-        Request ROVA calendar items
-        """
-        url = 'https://www.rova.nl/api/TrashCalendar/GetCalendarItems'
-
-        # request data from rova server and return response in json
-        response = requests.get(url, params={'portal': 'inwoners'}, cookies=self.get_cookies())
-
-        response.raise_for_status()
-
-        rova_response = response.json()
-
-        items = []
-        types = []
-
-        # add next pickup date for each garbage type
-        for item in rova_response:
-            date = datetime.strptime(item["Date"], "%Y-%m-%dT%H:%M:%S")
-            garbage_type = item["GarbageTypeCode"].upper()
-
-            if garbage_type not in types:
-                items.append({
-                    'GarbageTypeCode': garbage_type,
-                    'Date': date.strftime("%Y-%m-%dT%H:%M:%S")
-                })
-                types.append(garbage_type)
-
-        return items
-
-    def get_cookies(self):
-        """Generate cookies for ROVA API request"""
-        return {'RovaLc_inwoners': "{{'Id':{},'ZipCode':'{}', \
-        'HouseNumber':'{}', 'HouseAddition':'{}','Municipality':'', \
-        'Province':'', 'Firstname':'','Lastname':'','UserAgent':'', \
-        'School':'', 'Street':'','Country':'','Portal':'', \
-        'Lat':'','Lng':'', 'AreaLevel':'','City':'','Ip':''}}"
-        .format(self.rova_id, self.zip_code, self.house_number, self.house_addition)}
-
-
-class RovaSourceAPI(AbstractRovaSource):
-    """
-    Object using ROVA's app API-methods
-    https://play.google.com/store/apps/details?id=nl.quintor.mip.android&hl=en
-    https://www.rova.nl/inwoners/faq/600/detail/
-    """
-
-    API_KEY = '5ef443e778f41c4f75c69459eea6e6ae0c2d92de729aa0fc61653815fbd6a8ca'
-
-    def __init__(self, zip_code, house_number, house_addition):
-        """
-        The current API_KEY from the ROVA app will be used
-        """
-        super().__init__(zip_code, house_number, house_addition)
-
-        self.api_key = self.API_KEY
-
-    def is_rova_area(self):
-        """
-        Check if ROVA collects garbage at this address
-        """
-        url = 'http://api.inzamelkalender.rova.nl/webservices/appsinput/'
+        url = 'https://www.rova.nl/api/waste-calendar/upcoming'
 
         # request data from rova api and check if rova collects garbage at this address
+        # requesting with a non-existing postalcode will result in a error message
+
         response = requests.get(url, params = {
-            'apikey': self.api_key,
-            'method': 'tinyPostcodeCheck',
-            'postcode': self.zip_code,
-            'huisnummer': self.house_number,
-            'toevoeging':self.house_addition
+            'postalcode': self.zip_code,
+            'houseNumber': self.house_number,
+            'addition' : self.house_addition,
+            'take' :    '1',
             })
 
         response.raise_for_status()
 
         rova_response = response.text.strip()
-
+        if rova_response != '[]':
+            rova_response = "OK"
         return rova_response == "OK"
 
     def get_calendar_items(self):
         """
         Get next pickup date for each garbage types
         """
-        url = 'http://api.inzamelkalender.rova.nl/webservices/appsinput/'
-
-        # request data from rova API and save response in year items
+        url = 'https://www.rova.nl/api/waste-calendar/upcoming'
+        # request data from rova API and save response first 5 items (default)
         response = requests.get(url, params = {
-            'toevoeging' : self.house_addition,
-            'huisnummer': self.house_number,
-            'postcode': self.zip_code,
-            'apikey': self.api_key,
-            'method': 'postcodecheck',
-            'platform': 'pc',
-            'street': '',
+            'postalcode': self.zip_code,
+            'houseNumber': self.house_number,
+            'addition' : self.house_addition,
+            'take' :    self.take,
             })
 
         response.raise_for_status()
 
         rova_response = response.json()
-
-        today = datetime.now()
-
+        
         items = []
         types = []
-
         # add next pickup date for each garbage type
-        for item in rova_response['data']['ophaaldagen']['data']:
-            date = datetime.strptime(item["date"], "%Y-%m-%d")
-            garbage_type = item["type"].upper()
+        for item in rova_response:
+            date = datetime.strptime(item["date"], "%Y-%m-%dT%H:%M:%SZ")
+            date = date.strftime("%Y-%m-%dT%H:%M:%S")
+            garbage_type = item["garbageTypeCode"].upper()
 
-            # only add pickup dates in the future
-            if date >= today and garbage_type not in types:
-                items.append({
-                    'GarbageTypeCode': garbage_type,
-                    'Date': date.strftime("%Y-%m-%dT%H:%M:%S")
+            items.append({
+                'GarbageTypeCode': garbage_type,
+                'Date': date
                 })
-                types.append(garbage_type)
-
+            types.append(garbage_type)            
         return items
